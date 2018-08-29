c5bd083d6e0ac2b25a828f1642ec62d446d6f76e
Simon Babay <simon@babay.fr>
2018-08-29 22:51:47 +0200

    Updates for 0.3.20

--- src/plugins/navLoader.js
+++ src/plugins/navLoader.js
@@ -1,42 +1,44 @@
-import Vue from "vue";
-import VueProgressBar from "vue-progressbar";
+import Vue from 'vue';
+import VueProgressBar from 'vue-progressbar';
 
 /**
  * Simple plugin to show a loader on pages changes
  */
 Vue.use(VueProgressBar, {
-  color: "#2E81ED",
-  failedColor: "#FF0000",
-  thickness: "5px"
+  color: '#2E81ED',
+  failedColor: '#FF0000',
+  thickness: '5px'
 });
 
-export default async ({ app, router, store }) => {
-  // On boot: start progress bar
-  app.$Progress.start();
-
-  // When a route change
-  router.beforeResolve((to, from, next) => {
-    if (to.meta.progress !== undefined) {
-      let meta = to.meta.progress;
-      app.$Progress.parseMeta(meta);
-    }
-    // Start progress
+export default async ({ appCreated, router, store }) => {
+  appCreated(app => {
+    // On boot: start progress bar
     app.$Progress.start();
-    next();
-  });
 
-  // When route changed
-  router.afterEach((to, from) => {
-    app.$Progress.finish();
-  });
+    // When route has a redirect
+    app.$on('router.redirect', () => {
+      app.$Progress.finish();
+    });
 
-  // When route has a redirect
-  app.$on("router.redirect", () => {
-    app.$Progress.finish();
-  });
+    // When a route has an error
+    app.$on('router.error', () => {
+      app.$Progress.fail();
+    });
 
-  // When a route has an error
-  app.$on("router.error", () => {
-    app.$Progress.fail();
+    // When a route change
+    router.beforeResolve((to, from, next) => {
+      if (to.meta.progress !== undefined) {
+        let meta = to.meta.progress;
+        app.$Progress.parseMeta(meta);
+      }
+      // Start progress
+      app.$Progress.start();
+      next();
+    });
+
+    // When route changed
+    router.afterEach((to, from) => {
+      app.$Progress.finish();
+    });
   });
 };
--- src/plugins/spaLoader.js
+++ src/plugins/spaLoader.js
@@ -1,14 +1,16 @@
 /**
  * Plugin to remove loader when everything is ready on SPA routes
  */
-export default async ({ app }) => {
-  // Add a callback on main app mounted() hook
-  app.$on("app.mounted", () => {
-    // Find loader
-    const loader = document.querySelector(".spa-loading");
-    if (loader) {
-      // Remove it
-      loader.remove();
-    }
+export default async ({ appCreated }) => {
+  appCreated(app => {
+    // Add a callback on main app mounted() hook
+    app.$on('app.mounted', () => {
+      // Find loader
+      const loader = document.querySelector('.spa-loading');
+      if (loader) {
+        // Remove it
+        loader.remove();
+      }
+    });
   });
 };
